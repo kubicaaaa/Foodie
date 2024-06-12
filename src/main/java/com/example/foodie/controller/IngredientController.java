e90d246dbc21f171565cf12a9870d3be6f805f3b
kubicaaa <chabrowski31@gmail.com>
2024-06-12 19:52:24 +0200

    logic changes

--- src/main/java/com/example/foodie/controller/IngredientController.java
+++
@@ -1,4 +0,0 @@
-package com.example.foodie.controller;
-
-public class IngredientController {
-}
--- src/main/java/com/example/foodie/repository/IngredientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.foodie.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class IngredientRepository {
-}
